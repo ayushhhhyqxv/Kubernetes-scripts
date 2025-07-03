4abeab8f80aa3d6db32fcc19e5c92ca86e66d24e
Ayush Andure <ayushpadhaikar@gmail.com>
2025-07-03 22:53:55 +0530

    for local K8S

---
+++ commands.sh
@@ -0,0 +1,46 @@
+# Download Minikube for linux first
+
+sudo usermod -a -G docker $USER && newgrp docker # Give permission to docker for driver 
+
+minikube start --driver=docker
+
+docker ps # Check whether minikube is running or not
+
+minikube ssh
+
+docker ps # check all the kubernetes services here,then exit 
+
+sudo snap install kubectl --classic # snap is a tool to install containerized application
+
+kubectl get pods # will show pods having default names
+
+kubectl get nodes # will show nodes aligned
+
+kubectl get pods --namespace=kube-system  # shows all k8s system pods
+
+kubectl get namespaces  # would specify types of namespaces
+
+kubectl create namespace <your-app-name>
+
+# Check again it would be reflected ! 
+
+# In order to create a pod, write a yaml file
+
+# once done with yaml file then 
+
+kubectl apply -f pod.yaml
+
+kubectl get pods --namespace=<your-namespace>  # will show your running pod 
+
+minikube ssh 
+
+docker ps # can now see running your container
+
+kubectl get po --namespace=<your-namespace> -o wide # get pods with specified namespace in wide output with IP
+
+kubectl delete -f pod.yaml  # to delete the entire pod,it does not delete the file / we can even edit for now i am editing some changes 
+
+# Adding more containers to a single pod 
+
+
+
